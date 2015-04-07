e5baf0932f4c3614a1ebdd0113d68745c46bf3ea
David Dworken <david@daviddworken.com>
2015-04-06 20:05:28 -0400

    Create authenticate.py

---
+++ parts/authenticate.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, g, redirect, url_for
+app = Flask(__name__)
+
+from flaskext.auth import Auth, AuthUser, login_required, logout
+auth = Auth(app, login_url_name='index')
+
+#Change the usernames, passwords, and salts!
+creds = [{'username':'admin',   'password':'password', 'salt':'231', 'level':'control' },
+         {'username':'control', 'password':'password', 'salt':'925', 'level':'control' },
+         {'username':'data',    'password':'password', 'salt':'761', 'level':'dataOnly'}
+        ]
+
+@app.before_request
+def init_users():
+    g.control = {}
+    g.dataOnly = {}
+    for dict in creds:
+        user = AuthUser(username=dict['username'])
+        user.set_and_encrypt_password(dict['password'], salt=dict['salt'])
+        if dict['level'] == 'control':
+            g.control[dict['username']] = user
+        if dict['level'] == 'dataOnly':
+            g.dataOnly[dict['username']] = user
+
+@login_required()
+def admin():
+    return '''Authenticated to control the drone. <br> 
+              <a href="/logout/">Log Out</a>'''
+
+@login_required()
+def dataOnly():
+    return '''Authenticated for data. <br>
+              <a href="/logout/">Log Out</a>'''
+
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        if username in g.control:
+            if g.control[username].authenticate(request.form['password']):
+                return redirect(url_for('admin'))
+        if username in g.dataOnly:
+            if g.dataOnly[username].authenticate(request.form['password']):
+                return redirect(url_for('dataOnly'))
+        return 'Failure :('
+    return '''
+            Please authenticate to control the AR Drone. 
+            <form method="POST">
+                Username: <input type="text" name="username"/><br/>
+                Password: <input type="password" name="password"/><br/>
+                <input type="submit" value="Log in"/>
+            </form>
+        '''
+
+def logout_view():
+    user_data = logout()
+    if user_data is None:
+        return 'No user to log out.'
+    return 'Logged out user {0}.'.format(user_data['username'])
+
+# URLs
+app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
+app.add_url_rule('/admin/', 'admin', admin)
+app.add_url_rule('/dataOnly', 'dataOnly', dataOnly)
+app.add_url_rule('/logout/', 'logout', logout_view)
+
+# Secret key needed to use sessions.
+app.secret_key = '9b9777081187a5254ac5a4c2018e775d'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+david@DAVID-6120-SERVER:~/summerResearch$ 
